bb382347a340594f6ccdc5d341bbc286ecb04b03
Toqua Magdy <toquamagdy25@gmail.com>
2024-06-23 23:22:05 +0300

    custom App Bar

--- lib/features/home/presentation/views/widgets/home_header.dart
+++ lib/features/home/presentation/views/widgets/home_header.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:store_app/core/utils/app_styles.dart';
 
-class HomeHeader extends StatelessWidget {
+class HomeHeader extends StatelessWidget implements PreferredSizeWidget {
   const HomeHeader({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+   return AppBar(
+     backgroundColor: Colors.white,
+     title: Padding(
+       padding: const EdgeInsets.symmetric(horizontal:  10),
+       child: Row(
+         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+         children: [
+           Text('Slash.' ,style:AppStyles.styleBold20),
+           Row(
+             mainAxisAlignment: MainAxisAlignment.center,
+             children: [
+               Image.asset('assets/images/location.png'),
+               const SizedBox(width: 5),
+               Column(
+                 crossAxisAlignment: CrossAxisAlignment.start,
+                 children: [
+                   Text('Nasr City',style: AppStyles.styleRegular14,),
+                   Text('Cairo',style: AppStyles.styleBold14,)
+                 ],
+               ) ,
+               const SizedBox(width: 7),
+               Transform.rotate(
+                   angle: -1.571,
+                   child: const Icon(Icons.arrow_back_ios_new_outlined ,size:
+                   20,)) ,
+             ],
+           ),
+           Image.asset('assets/images/Notifcation_Icon.png'),
+         ],
+       ),
+     ),
+   );
   }
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
