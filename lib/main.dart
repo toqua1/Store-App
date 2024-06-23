957aba67ce5ee86168535f47a46a15b0a375c30a
Toqua Magdy <toquamagdy25@gmail.com>
2024-06-23 23:27:31 +0300

    Last update

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:store_app/features/home/presentation/views/home_screen.dart';
-// import 'package:store_app/features/home/presentation/views/widgets/navBar.dart';
-//
-// void main() {
-//   runApp(const MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home:NavBar(),
-//     );
-//   }
-// }
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:store_app/features/home/data/repositories/product_repository.dart';
