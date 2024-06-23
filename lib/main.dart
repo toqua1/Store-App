d64267be0b45a982d3a90e3efc3aecc765c8186c
Toqua Magdy <toquamagdy25@gmail.com>
2024-06-23 23:23:06 +0300

    images

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,35 @@
+// import 'package:flutter/material.dart';
+// import 'package:store_app/features/home/presentation/views/home_screen.dart';
+// import 'package:store_app/features/home/presentation/views/widgets/navBar.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       home:NavBar(),
+//     );
+//   }
+// }
+
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:store_app/features/home/data/repositories/product_repository.dart';
+import 'package:store_app/features/home/presentation/views/home_screen.dart';
+import 'package:store_app/features/home/presentation/views/widgets/navBar.dart';
+import 'package:store_app/features/home/presentation/blocs/product_cubit.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,16 +38,23 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ProductCubit>(
+          create: (context) => ProductCubit(ProductRepository()),
+        ),
+        // Add other providers here if needed
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          useMaterial3: true,
+        ),
+        home: NavBar(),
       ),
-      home: const SizedBox(),
     );
   }
 }
